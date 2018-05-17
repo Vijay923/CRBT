5215ba84eb3025b904a24b26ee14e16bc5bab45e
vijayakumara <vijaykumarpatil923@gmail.com>
2018-05-17 23:12:04 +0530

    nakkan

--- modules/Fuck.pm
+++ modules/Fuck.pm
@@ -1,4 +1,15 @@
 package Fuck;
 print "package loaded successfully\n";
 
+sub new
+{
+	goto &fuck;
+}
+
+sub fuck
+{
+	$caller=caller();
+	print "actuall caller: $caller\n";
+	return "Fuckman";
+}
 1;
--- scripts/sample.pl
+++ scripts/sample.pl
@@ -2,3 +2,6 @@
 
 use lib "/home/vijayakumara/Test_git/CRBT/modules";
 use Fuck;
+
+$oh=new Fuck;
+print "oh : $oh\n";
